0b336d5ac0def06ed82bc451e240ee6ed7d474dd
Ebrahim Aly <ebrahim.aly@gmail.com>
2020-10-03 19:10:32 -0400

    Commiting CSharpApp2

---
+++ SimpleCSharpApp2/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace simplecsharpapp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Display a simple message to the user.
+            Console.WriteLine("***** My First C# App ******");
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("****************************");
+            Console.WriteLine();
+
+            // Wait for Enter key to be pressed beffore shutting down.
+            Console.ReadLine();
+        }
+    }
+}
