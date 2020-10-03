2f817244475d4fc4a40676ef2dc00c0874abcace
Ebrahim Aly <ebrahim.aly@gmail.com>
2020-10-03 15:46:25 -0400

    SimpleCSharpApp commit

---
+++ SimpleCSharpApp/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace simplecsharpapp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Display a simple message to the user.
+            Console.WriteLine("***** My First C# App ******");
+            Console.WriteLine("Hello World!");
+            Console.WriteLine();
+
+            // Wait for Enter key to be pressed beffore shutting down.
+            Console.ReadLine();
+        }
+    }
+}
